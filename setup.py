550995b88d04efaad6a24882b8a6b9c4b14ab20b
Sergio Mora <samora@poligran.edu.co>
2021-10-11 18:53:22 -0500

    fix release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         "License :: OSI Approved :: MIT License",
