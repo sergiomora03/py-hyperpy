9c802bb9de043ebde1835553a51279c81a4c1746
Sergio Mora <samora@poligran.edu.co>
2021-12-20 00:31:15 -0500

    update  .readthedocs.yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,6 @@ import sys, os
 # ? for mod_name in MOCK_MODULES:
 # ?     sys.modules[mod_name] = mock.Mock()
 
-sys.path.append(os.path.abspath('requirements.txt'))
-
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.abspath('sphinxext'))
 
